5ef2b3cb1e56cba8fc4226a580fc4d8291d443ba
sebgoa <runseb@gmail.com>
2017-10-25 15:33:34 +0200

    first dump

---
+++ README.d
@@ -0,0 +1,53 @@
+# DevOps Days Tel-Aviv
+
+In couple weeks, I am heading to Tel-Aviv to give the opening Keynote. A trick played by Sharone Zitzman for sure and a clear case of impostor syndrome.
+
+So how about you (whoever will find this site), help me with it.
+
+## The Title
+
+** How can we survive continuous innovation ? **
+
+## The Abstract
+
+It certainly feels like life is speeding up, that software innovation is accelerating. Millions of us are on GitHub sharing code, millions of repositories and commits in front of us for everyone to see. While open source was for a long time a niche in the industry, its DNA is changing rapidly as software vendors realize that open source is a competitive weapon. The end result of an accelerating open sourcing and open competition is that we are left with dozens upon dozens of choices. Whether it is to choose a cloud solution, a storage solution, a container orchestrator etc. Then we are also left with changing paradigms, with pundits telling us that the fame of yesterday is dead tomorrow and that there is a new king on the block. So what do we do ? How can our enterprise survive ? How can we adapt, when before we wake up the new technology has already been taken over by the new shiny toy.
+We don’t need continuous integration, we need continuous adaption for this continuously innovating world. Yes it is about culture, it is about being aware that our environment will change and that we need to evolve to survive.
+
+In this talk, I will review this trends and highlight the problems they present, then I will show some resources that I use to be able to navigate this world and keep myself in the loop.
+
+## Discussion
+
+So basically, I am pondering on the three following things:
+
+* the software and tooling landscape is exploding and it is "untenable" (maybe not the right word)
+
+You have [this](https://blog.profitbricks.com/wp-content/uploads/2015/12/infrastructure-automation-ecosystem-landscape-infographic.jpg)
+You have the [docker ecosystem](https://www.ctl.io/developers/assets/images/blog/docker-ecosystem.jpg)
+And now you have the [CNCF landscape](https://github.com/cncf/landscape/blob/master/landscape/CloudNativeLandscape_latest.jpg)
+
+How do we make a choice ?
+Which tools are good ?
+Which tools will last ?
+How do I keep up and learn these tools ?
+
+* ["Open source is being eaten by Cloud"](https://image.slidesharecdn.com/copyofcncfkeynote-whatiscloudnative-170406143435/95/cncf-keynote-what-is-cloud-native-34-638.jpg?cb=1491489801)
+
+All IT vendors seem to have embraced Open Source and are contributing a lot of new tools to the arsenal. Yet the core services of Cloud are not open sourced, so has open source become a classical case of bait and switch.
+
+* Innovation is increasingly driven by corporation.
+
+Certainly academic research is alive and well (to some extent), but the number of great students that leave Ph-D programs for corporations is increasing (need number, just a guess :) )
+The combined R&D budget of Google,Amazon,Facebook,Apple is now close to $100B per year, which is higher than the R&D budget of France (especially when you take out the defence budget)
+
+Brain drain used to be from uni to countries, now it is from uni to corporations.
+
+They all could be correlated, more R&D from corporation, being done in the open triggers more available tools (impossible to figure out ?)
+
+## How do we survive ?
+
+* Go back to "No silver bullet" from Fred Brooks. Stop treating those tools as silver bullets, and ask marketeers to stop pretending they are.
+* Situational awareness but emotional detachment from "silver bullets"
+* Devops culture to adapt, self-organize and deliver value, tools do not matter (basic principle of agile).
+
+
+
